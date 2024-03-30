fb251f123bbd9cc1f608c892c0cf8fdb7894731e
Snizhana Kostiuk <snizhana2812kostyuk@gmail.com>
2024-03-30 22:19:22 +0200

    Done home task

--- TestUnit/UserValidationTests.cs
+++ TestUnit/UserValidationTests.cs
@@ -10,6 +10,36 @@ namespace TestUnit
         [InlineData(null)]
         [InlineData("")]
         [InlineData(" ")]
+        public void UserValidation_IsNicknameValid_InvalidData(string nickname)
+        {
+            Assert.Throws<ArgumentException>(() => UserValidation.isNicknameValid(nickname));
+        }
+
+        [Theory]
+        [InlineData("Nickname!@#")]
+        [InlineData("4324fds+")]
+        public void UserValidation_IsNicknameValid_InvalidNickname(string nickname)
+        {
+            bool result = UserValidation.isNicknameValid(nickname);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void UserValidation_IsNicknameValid_ValidNickaname()
+        {
+            //Triple A (Arrange, Act, Assert)
+            //Arrange
+            string nickname = "myNickname_";
+            //Act
+            bool result = UserValidation.isNicknameValid(nickname);
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
         public void UserValidation_IsNameValid_InvalidData(string name)
         {
             Assert.Throws<ArgumentException>(() => UserValidation.isNameValid(name));
@@ -37,5 +67,74 @@ namespace TestUnit
 
             //Assert.Throws<NotImplementedException>(() => UserValidation.isNameValid(name));
         }
+
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void UserValidation_Is_EmailValid_InvalidData(string email)
+        {
+            Assert.Throws<ArgumentException>(() => UserValidation.isEmailValid(email));
+        }
+
+
+
+        [Theory]
+        [InlineData("admin@com")]
+        [InlineData("admin.com")]
+        public void UserValidation_Is_EmailValid_InvalidEmail(string email)
+        {
+            bool result = UserValidation.isEmailValid(email);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void UserValidation_IsEmailValid_ValidEmail()
+        {
+            //Triple A (Arrange, Act, Assert)
+            //Arrange
+            string email = "test_email123@test.com";
+            //Act
+            bool result = UserValidation.isEmailValid(email);
+            //Assert
+            Assert.True(result);
+
+
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void UserValidation_Is_PasswordValid_InvalidData(string password)
+        {
+            Assert.Throws<ArgumentException>(() => UserValidation.isPasswordValid(password));
+        }
+
+
+
+        [Theory]
+        [InlineData("rewiirewrIOrerewhr")]
+        [InlineData("4356gfdgfdgtretret")]
+        public void UserValidation_Is_PasswordValid_InvalidPassword(string password)
+        {
+            bool result = UserValidation.isPasswordValid(password);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void UserValidation_IsPasswordValid_ValidPassword()
+        {
+            //Triple A (Arrange, Act, Assert)
+            //Arrange
+            string password = "251208asdfG";
+            //Act
+            bool result = UserValidation.isPasswordValid(password);
+            //Assert
+            Assert.True(result);
+
+        }
+
     }
 }
--- TestUnitProject/UserValidation.cs
+++ TestUnitProject/UserValidation.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace TestUnitProject
 {
     public static class UserValidation
     {
+
+        public static bool isNicknameValid(string nickname)
+        {
+            if (string.IsNullOrWhiteSpace(nickname))
+            {
+                throw new ArgumentException("Nickname cannot be null or whitespace.", nameof(nickname));
+            }
+            string nicknamePattern = @"^[a-zA-Z0-9_]+$";
+            Match match = Regex.Match(nickname, nicknamePattern);
+            return match.Success;
+
+        }
+
         public static bool isNameValid(string name)
         {
             if (string.IsNullOrWhiteSpace(name))
@@ -18,14 +28,40 @@ namespace TestUnitProject
 
         }
 
-        public static bool isEmailValid(string password)
+        public static bool isEmailValid(string email)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentException("Email cannot be null or whitespace.", nameof(email));
+            }
+            string emailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            Match match = Regex.Match(email, emailPattern);
+            return match.Success;
+
         }
 
         public static bool isPasswordValid(string password)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Password cannot be null or whitespace.", nameof(password));
+            }
+            if (password.Length <= 8)
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[A-Z]"))
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[0-9]"))
+            {
+                return false;
+            }
+            return true;
+
         }
     }
 }
